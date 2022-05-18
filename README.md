2858b3a4171572286c58900708b0aa86f47194a1
0xbe1 <0xbetrue@gmail.com>
2022-05-18 14:18:19 +0800

    add store_market

--- README.md
+++ README.md
@@ -9,8 +9,7 @@ substreams run -e api-dev.streamingfast.io:443 substream.yaml handle_tokens
 
 ## TODOs
 
-- [ ] save Market
 - [ ] store LendingProtocol (able to run .load)
 - [ ] handle NewPriceOracle (verify .load works)
-- [ ] fetch underlying from CToken (Token and Market ready)
+- [ ] also fetch underlying from CToken (Token and Market ready)
 - [ ] handle mint, redeem, borrow, repay borrow, and liquidate borrow (leverage Token and Market)
--- src/lib.rs
+++ src/lib.rs
@@ -83,6 +83,47 @@ pub extern "C" fn store_tokens(block_ptr: *mut u8, block_len: usize) {
     }
 }
 
+// TODO: almost identical to store_market,
+// any chance i can store 2 entities, but in the same module?
+#[no_mangle]
+pub extern "C" fn store_market(block_ptr: *mut u8, block_len: usize) {
+    substreams::register_panic_hook();
+
+    let blk: pb::eth::Block = proto::decode_ptr(block_ptr, block_len).unwrap();
+
+    for trx in blk.transaction_traces {
+        // Unitroller address
+        if hex::encode(&trx.to) != "3d9819210a31b4961b30ef54be2aed79b9c9cd3b" {
+            continue;
+        }
+        for call in trx.calls {
+            if call.state_reverted {
+                continue;
+            }
+
+            call.logs
+                .iter()
+                .filter(|log| utils::is_market_listed_event(log))
+                .for_each(|log| {
+                    let addr = &address_pretty(&log.data[12..32].to_vec());
+                    // TODO: can i save this call?
+                    let c_token = rpc::retry_rpc_calls(addr);
+                    let market = Market {
+                        id: addr.to_string(),
+                        name: c_token.name,
+                        input_token_address: "todo".to_string(),
+                        output_token_address: addr.to_string(),
+                    };
+                    state::set_if_not_exists(
+                        1,
+                        format!("market:{}", market.id),
+                        &proto::encode(&market).unwrap(),
+                    );
+                });
+        }
+    }
+}
+
 /// Find and output all the ERC20 transfers
 ///
 /// `block_ptr`: Pointer of where the block is located in the wasm heap memory
